6c3fb369ff48b560f10198cc68b9188b26cdf0fa
Femi Opanuga <femathic@gmail.com>
2020-02-05 08:48:51 -0800

    Edit App.test.js to fit the new folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from "react-redux";
-import { configureStore } from "../store";
+import { configureStore } from "./store";
 
 const store = configureStore();
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/tailwind.css';
-import App from './components/App';
+import './styles/main.scss';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
